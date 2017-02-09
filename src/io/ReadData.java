cd33fc39e5e3755589290246a557f81e732c39de
AlexanderKomi <Alexander.Komischke@gmx.de>
2017-02-09 23:35:21 +0100

    Just some formating.

--- src/io/ReadData.java
+++ src/io/ReadData.java
@@ -11,7 +11,9 @@ import model.ressources.Skill;
 import model.ressources.Unit;
 
 /**
- * Still WIP. <br><br>This class reads all the data provided for this program. <br>
+ * Still WIP. <br>
+ * <br>
+ * This class reads all the data provided for this program. <br>
  * The read data will be put in variables, which can be used for further
  * use.<br>
  * When the data is purified and ready for use, mostly it can be accessed as
@@ -19,58 +21,52 @@ import model.ressources.Unit;
  * 
  * @author Alexander Komischke
  */
-public class ReadData{
+public class ReadData {
 
-	private String			game;
-	private String			_csv_split_by	= ",";
-	private BufferedReader	br;
+	private String game;
+	private String _csv_split_by = ",";
+	private BufferedReader br;
 
-	List <String>			locations;
-	List <String>			attributes_as_data;
-	List <String>			skills_as_data;
-	List <List <String>>	units_as_data;
+	List<String> locations;
+	List<String> attributes_as_data;
+	List<String> skills_as_data;
+	List<List<String>> units_as_data;
 
-	List <Attribute>	attributes;
-	List <Skill>		skills;
-	List <Unit>			units;
+	List<Attribute> attributes;
+	List<Skill> skills;
+	List<Unit> units;
 
 	/**
-	 * Still WIP. <br><br>
+	 * Still WIP. <br>
+	 * <br>
 	 * Reads the Data from the Files<br>
 	 * 
 	 * @param String
 	 *            Gets as parameter the game used. The Game must be verified.
 	 */
-	public ReadData( String game ) {
+	public ReadData(String game) {
 
-		this.setGame( game );
+		this.setGame(game);
 
-		locations = new ArrayList <String>();
+		locations = new ArrayList<String>();
 
-		attributes_as_data = new ArrayList <>();
-		skills_as_data = new ArrayList <>();
-		units_as_data = new ArrayList <>();
+		attributes_as_data = new ArrayList<>();
+		skills_as_data = new ArrayList<>();
+		units_as_data = new ArrayList<>();
 
-		attributes = new ArrayList <Attribute>();
-		skills = new ArrayList <Skill>();
-		units = new ArrayList <Unit>();
+		attributes = new ArrayList<Attribute>();
+		skills = new ArrayList<Skill>();
+		units = new ArrayList<Unit>();
 
-		locations = getFileLocations( new java.io.File( "data/" + getGame() + "/" ).getAbsolutePath() );
-		readData( locations );
+		locations = getFileLocations(new java.io.File("data/" + getGame() + "/").getAbsolutePath());
+		readData(locations);
 
 	}
 
 	/**
-	 * Puts all the filtered data in variables. <br>The variables can be used for
-	 * further use.
+	 * Puts all the filtered data in variables. <br>
+	 * The variables can be used for further use.
 	 */
-	
-
-
-
-
-
-
 
 	/**
 	 * Reads the Data from the paths in locations and writes the result in the
@@ -79,43 +75,41 @@ public class ReadData{
 	 * @param List<String>
 	 *            contains the full path to the file.
 	 */
-	public void readData( List <String> locations ) {
+	public void readData(List<String> locations) {
 
 		String line = "";
-		if ( locations == null ) {
+		if (locations == null) {
 			return;
 		}
 
 		for (String s : locations) {
-			List <String> content = new ArrayList <String>();
+			List<String> content = new ArrayList<String>();
 			try {
-				br = new BufferedReader( new FileReader( s ) );
+				br = new BufferedReader(new FileReader(s));
 
-				while (( line = br.readLine() ) != null) {
-					String[] inputFromFile = line.split( _csv_split_by );
+				while ((line = br.readLine()) != null) {
+					String[] inputFromFile = line.split(_csv_split_by);
 					for (String inFile : inputFromFile) {
 
 						// TO DO DATA FROM FILES IN VARIABLES -> INSERT CODE
 						// HERE
 
-						if ( s.endsWith( "Skills.csv" ) ) {
+						if (s.endsWith("Skills.csv")) {
 							// works
-							skills_as_data.add( inFile.trim() );
-						}
-						else if ( s.endsWith( "Attribute.csv" ) ) {
+							skills_as_data.add(inFile.trim());
+						} else if (s.endsWith("Attribute.csv")) {
 							// works
-							attributes_as_data.add( inFile.trim() );
-						}
-						else if ( s.contains( "Pen and Paper\\data\\" + this.game + "\\player\\" ) ) {
+							attributes_as_data.add(inFile.trim());
+						} else if (s.contains("Pen and Paper\\data\\" + this.game + "\\player\\")) {
 							// TODO: in Progress
-							content.add( inFile.trim() );
+							content.add(inFile.trim());
 
 						}
 
 					}
-					if ( !content.isEmpty() ) {
-						if ( !units_as_data.contains( content ) ) {
-							units_as_data.add( content );
+					if (!content.isEmpty()) {
+						if (!units_as_data.contains(content)) {
+							units_as_data.add(content);
 						}
 					}
 				}
@@ -134,22 +128,21 @@ public class ReadData{
 	 * @return Returns a List<String> with the paths to the files, that are
 	 *         available.
 	 */
-	public List <String> getFileLocations( String path ) {
+	public List<String> getFileLocations(String path) {
 
-		File directory = new File( path );
+		File directory = new File(path);
 		File[] list = directory.listFiles();
 
-		if ( list == null ) {
+		if (list == null) {
 			return null;
 		}
 
 		for (File f : list) {
-			if ( f.isDirectory() ) {
+			if (f.isDirectory()) {
 				path = f.getAbsolutePath();
-				getFileLocations( path );
-			}
-			else {
-				locations.add( f.getAbsolutePath() );
+				getFileLocations(path);
+			} else {
+				locations.add(f.getAbsolutePath());
 			}
 		}
 
@@ -161,29 +154,29 @@ public class ReadData{
 	 */
 	public void printLocationsFromFiles() {
 
-		if ( locations == null ) {
-			System.out.println( "Locations : No data found." );
+		if (locations == null) {
+			System.out.println("Locations : No data found.");
 			return;
 		}
 
-		System.out.println( "\nLocations : " );
+		System.out.println("\nLocations : ");
 
 		for (String s : locations) {
-			System.out.println( s );
+			System.out.println(s);
 		}
 	}
 
 	public void printUnitsFromData() {
 
-		if ( units_as_data == null ) {
-			System.out.println( "units from data: No data found." );
+		if (units_as_data == null) {
+			System.out.println("units from data: No data found.");
 			return;
 		}
 
-		System.out.println( "\nUnits from data: " );
+		System.out.println("\nUnits from data: ");
 
-		for (List <String> s : units_as_data) {
-			System.out.println( s );
+		for (List<String> s : units_as_data) {
+			System.out.println(s);
 		}
 	}
 
@@ -192,29 +185,29 @@ public class ReadData{
 	 */
 	public void printAttsFromData() {
 
-		if ( attributes_as_data == null ) {
-			System.out.println( "atts from data: No data found." );
+		if (attributes_as_data == null) {
+			System.out.println("atts from data: No data found.");
 			return;
 		}
 
-		System.out.println( "\nAttribute from data: " );
+		System.out.println("\nAttribute from data: ");
 
 		for (String s : attributes_as_data) {
-			System.out.println( s );
+			System.out.println(s);
 		}
 	}
 
 	public void printAtts() {
 
-		if ( attributes == null ) {
-			System.out.println( "atts : No data found." );
+		if (attributes == null) {
+			System.out.println("atts : No data found.");
 			return;
 		}
 
-		System.out.println( "\nAttribute : " );
+		System.out.println("\nAttribute : ");
 
 		for (Attribute s : attributes) {
-			System.out.println( s.toString() );
+			System.out.println(s.toString());
 		}
 	}
 
@@ -223,41 +216,41 @@ public class ReadData{
 	 */
 	public void printSkillsFromData() {
 
-		if ( skills_as_data == null ) {
-			System.out.println( "skills from data : No data found." );
+		if (skills_as_data == null) {
+			System.out.println("skills from data : No data found.");
 			return;
 		}
 
-		System.out.println( "\nSkills from data: " );
+		System.out.println("\nSkills from data: ");
 
 		for (String s : skills_as_data) {
-			System.out.println( s );
+			System.out.println(s);
 		}
 	}
 
 	public void printSkills() {
 
-		if ( skills == null ) {
-			System.out.println( "skills : No data found." );
+		if (skills == null) {
+			System.out.println("skills : No data found.");
 			return;
 		}
 
-		System.out.println( "\nSkills : " );
+		System.out.println("\nSkills : ");
 
 		for (Skill s : skills) {
-			System.out.println( s.toString() );
+			System.out.println(s.toString());
 		}
 
-		System.out.println( "No more Skills" );
+		System.out.println("No more Skills");
 	}
 
 	public void printUnits() {
-		System.out.println( "Units : \n" );
+		System.out.println("Units : \n");
 		;
 		for (Unit u : units) {
-			System.out.println( u.toString() );
+			System.out.println(u.toString());
 		}
-		System.out.println( "No more Units" );
+		System.out.println("No more Units");
 	}
 
 	/**
@@ -281,7 +274,7 @@ public class ReadData{
 		return _csv_split_by;
 	}
 
-	public void set_csv_split_by( String _csv_split_by ) {
+	public void set_csv_split_by(String _csv_split_by) {
 		this._csv_split_by = _csv_split_by;
 	}
 
@@ -289,32 +282,32 @@ public class ReadData{
 		return game;
 	}
 
-	public void setGame( String game ) {
+	public void setGame(String game) {
 		this.game = game;
 	}
 
-	public List <Skill> getSkills() {
+	public List<Skill> getSkills() {
 		return this.skills;
 	}
 
-	public List <Attribute> getAttributes() {
+	public List<Attribute> getAttributes() {
 		return this.attributes;
 	}
 
-	public List <Unit> getUnits() {
+	public List<Unit> getUnits() {
 		return this.units;
 	}
 
-	public List <String> get_attributes_as_data(){
+	public List<String> get_attributes_as_data() {
 		return this.attributes_as_data;
 	}
-	
-	public List <String> get_skills_as_data(){
+
+	public List<String> get_skills_as_data() {
 		return this.skills_as_data;
 	}
-	
-	public List<List<String>> get_units_as_data(){
-		return this.units_as_data; 
+
+	public List<List<String>> get_units_as_data() {
+		return this.units_as_data;
 	}
-	
+
 }
