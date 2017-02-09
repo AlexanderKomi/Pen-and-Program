3541e793ed6c8e2f1d8f8aaa6df9dbc4e7db3ca8
Niss <dsellemann@gmail.com>
2017-02-09 16:31:56 +0100

    Added javadoc to CalculatorGuiController.

--- src/gui/CalculatorGuiController.java
+++ src/gui/CalculatorGuiController.java
@@ -8,6 +8,9 @@ import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
 import javafx.scene.input.ScrollEvent;
 
+/**This class is for the gui and calculation.
+ * 
+ * */
 public class CalculatorGuiController {
 
     @FXML
