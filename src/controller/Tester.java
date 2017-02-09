0da985698c97c35ebe3ab73211ddb0da9dccb496
Universität <Universitt@AlexSerpent>
2017-02-09 03:51:12 +0100

    test commit

--- src/controller/Tester.java
+++ src/controller/Tester.java
@@ -10,7 +10,6 @@ public class Tester {
 	public Tester(){
 		System.out.println("\n IMPORTANT: Tester : FOR USE IN GAME, PLS GET RID OF THIS CLASS");
 		System.out.println("Test for git.");
-		System.out.println("Test for git.");
 		
 		//TEST METHODEN
 		//testDiceRoll();
