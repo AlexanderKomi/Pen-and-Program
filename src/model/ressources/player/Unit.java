28bf28df2f886e1b3862457b908698b2fa25f771
Michael <michael.christian.wagner@gmx.de>
2017-03-10 13:31:59 +0100

    Missing class from last commit.

--- src/model/ressources/player/Unit.java
+++ src/model/ressources/player/Unit.java
@@ -14,16 +14,16 @@ public class Unit {
 	private boolean player_status;
 	private String race;
 	private String name;
-	private List < Skill > skills;
-	private List < Attribute > atts;
+	private List<Skill> skills;
+	private List<Attribute> atts;
 
 	// CONSTRUCTORS
 
-	public Unit( String name, List < Skill > skills, List < Attribute > atts, boolean player ) {
-		this.setName( name );
-		this.setSkills( skills );
-		this.setAttributes( atts );
-		this.setPlayer( player );
+	public Unit(String name, List<Skill> skills, List<Attribute> atts, boolean player) {
+		this.setName(name);
+		this.setSkills(skills);
+		this.setAttributes(atts);
+		this.setPlayer(player);
 	}
 
 	// METHODES
@@ -35,14 +35,16 @@ public class Unit {
 		result += "Playerstatus : " + this.player_status + "\n";
 		result += "Unit : " + this.name + ", Attributes : \n";
 
-		for ( Attribute at : this.atts ) {
-			result += "\t" + at.toString() + "\n";
+		for (Attribute at : this.atts) {
+			if (at.getValue() != 0)
+				result += "\t" + at.toString() + "\n";
 		}
 
 		result += "Unit : " + this.name + ", Skills : \n";
 
-		for ( Skill sk : this.skills ) {
-			result += sk.toString() + "\n";
+		for (Skill sk : this.skills) {
+			if (sk.getValue() != 0)
+				result += sk.toString() + "\n";
 		}
 
 		return result;
@@ -54,7 +56,7 @@ public class Unit {
 		return player_status;
 	}
 
-	public void setPlayer( boolean player ) {
+	public void setPlayer(boolean player) {
 		this.player_status = player;
 	}
 
@@ -62,23 +64,23 @@ public class Unit {
 		return name;
 	}
 
-	public void setName( String name ) {
+	public void setName(String name) {
 		this.name = name;
 	}
 
-	public List < Skill > getSkills() {
+	public List<Skill> getSkills() {
 		return skills;
 	}
 
-	public void setSkills( List < Skill > skills ) {
+	public void setSkills(List<Skill> skills) {
 		this.skills = skills;
 	}
 
-	public List < Attribute > getAttributes() {
+	public List<Attribute> getAttributes() {
 		return atts;
 	}
 
-	public void setAttributes( List < Attribute > atts ) {
+	public void setAttributes(List<Attribute> atts) {
 		this.atts = atts;
 	}
 
@@ -86,7 +88,7 @@ public class Unit {
 		return race;
 	}
 
-	public void setRace( String race ) {
+	public void setRace(String race) {
 		this.race = race;
 	}
 
